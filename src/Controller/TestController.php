c8e443303bb7324f7cddeed9e618b37972e5c900
Kazk12 <kazuko43KO@gmail.com>
2025-07-13 15:24:55 +0200

    DONE: possibilité de voir la liste des livres sur lequelles ils peuvent discuter, et bouton de redirirection pour directement voir toutes les annonces par rapport a ce livre. Manque juste a mettre un bon nom de route et l utiliser correctemen

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -9,19 +9,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 final class TestController extends AbstractController{
     public function __construct(
         private RefererService $refererService,
         private UserService $userService,
     ) {}
+
     #[Route('/test', name: 'app_test')]
-    public function index(BookRepository $bookRepository): Response
+    public function index(BookRepository $bookRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $books = $bookRepository->findAll();
+        $query = $bookRepository->createQueryBuilder('b')->orderBy('b.createdAt', 'DESC');
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            9
+        );
 
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
-            'books' => $books,
+            'pagination' => $pagination,
         ]);
     }
 }
